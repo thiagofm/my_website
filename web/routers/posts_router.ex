990278e9c78352fc5b4ed10276ddec1d67be2d1f
Thiago Fernandes Massa <thiagown@gmail.com>
2013-12-23 21:42:31 -0200

    Added route to create post

--- web/routers/posts_router.ex
+++ web/routers/posts_router.ex
@@ -3,11 +3,17 @@ defmodule PostsRouter do
 
   prepare do
     conn.fetch([:cookies, :params])
+    conn = conn.resp_content_type("application/json")
   end
 
   get "/" do
-    # insert into post (title, comment) VALUES ('Hello world', 'Yeah!');
-    IO.inspect(PostQueries.all)
     conn.resp 200, JSON.generate(PostQueries.all)
+  end
+
+  post "/" do
+    post = Post.new(title: conn.params[:title], comment: conn.params[:comment])
+    Repository.create(post)
+
+    conn.resp 201, ""
   end
 end

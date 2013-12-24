db8f25ebae1f452aac47ce8e08f9bfc4cc1d4c68
Thiago Fernandes Massa <thiagown@gmail.com>
2013-12-23 23:58:54 -0200

    Added test to load post and having issues with the create post

--- lib/repository.ex
+++ lib/repository.ex
@@ -1,5 +1,5 @@
 defmodule Repository do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
-  def url, do: "ecto://horo@localhost/my_website_development"
+  def url, do: "ecto://horo@localhost/my_website_#{Mix.env}"
 end
--- test/routers/posts_router_test.exs
+++ test/routers/posts_router_test.exs
@@ -4,9 +4,29 @@ defmodule PostsRouterTest do
 
   @endpoint PostsRouter
 
+  setup do
+    # remove all posts
+    Repository.delete_all(Post)
+    :ok
+  end
+
   test "it loads posts" do
+    # Create post
+    post = Post.new(title: "Hello", comment: "World")
+    Repository.create(post)
+
+    # Make req
     conn = get("/")
-    assert conn.resp_body == "{}"
+    parsedJson = JSON.parse(conn.resp_body)
+    assert Enum.at(parsedJson, 0)["comment"] == "World"
+    assert Enum.at(parsedJson, 0)["title"] == "Hello"
     assert conn.status == 200
+  end
+
+  test "it creates posts" do
+    conn = Dynamo.Connection.Test.new(:POST, "/", "key1=value1&key2=value2")
+    conn = process(PostsRouter, conn, :POST, "/")
+    assert conn.resp_body == ""
+    assert conn.status == 201
   end
 end
